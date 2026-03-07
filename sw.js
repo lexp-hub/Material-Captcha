0c4f12966970082f69783853ddec7cdd68e576b6
lexphub <lex30tr@gmail.com>
2026-03-07 15:27:48 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', () => {
+  // network first, no special cache logic to keep it simple
+});
